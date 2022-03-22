be76318a6d1ed1399f5f3767b755b965a62ce90b
BunnnYZ <prihvi.howal10@gmail.com>
2022-03-22 14:39:46 +0530

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  a: number = 10;
+  b: number = 20;
+  total = this.a+this.b;
   title = 'app';
 }
